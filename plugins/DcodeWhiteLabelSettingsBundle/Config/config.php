936c09879bd43f0614877c19ebca252d406a20ca
Unknown <peter.osvath@d-code.hu>
2018-10-10 17:35:57 +0200

    WhiteLabelSettings

---
+++ plugins/DcodeWhiteLabelSettingsBundle/Config/config.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'name'        => 'Whitelabel',
+    'description' => 'Enables editing whitelabel information',
+    'author'      => 'peter.osvath@d-code.hu',
+    'version'     => '1.0.0',
+
+    'services' => [
+        'events' => [
+            'mautic.asset.configbundle.subscriber' => [
+                'class' => 'MauticPlugin\DcodeWhiteLabelSettingsBundle\EventListener\ConfigSubscriber',
+            ],
+        ],
+        'integrations' => [
+            'mautic.integration.rabbitmq' => [
+                'class'     => \MauticPlugin\RabbitMQBundle\Integration\WhiteLabelIntegration::class,
+                'arguments' => [
+                ],
+        	]
+    	]
+    ], 
+    'parameters' => [
+        'whitelabel_branding_name' => '',
+        'whitelabel_branding_version' => '',
+        'whitelabel_branding_copyright' => '',
+        'whitelabel_branding_favicon' => '',
+        'whitelabel_branding_apple_favicon' => '',
+        'whitelabel_branding_logo' => '',
+        'whitelabel_branding_left_logo' => '',
+    ],
+];
