e0a9fb2312425260a7ddc97c622e574016451afe
Unknown <peter.osvath@d-code.hu>
2018-11-15 14:59:57 +0100

    bounce callback handling

--- plugins/DcodeSenderEngineBundle/Controller/BounceCallbackController.php
+++ plugins/DcodeSenderEngineBundle/Controller/BounceCallbackController.php
@@ -21,7 +21,7 @@ class BounceCallbackController extends CommonController
 {
     public function callbackAction(Request $request)
     {
-        //$this->integrationHelper = $this->get('mautic.helper.integration');         
+        $this->integrationHelper = $this->get('mautic.helper.integration');         
         $integration = $this->integrationHelper->getIntegrationObject('SenderEngine');
         if (!$integration || $integration->getIntegrationSettings()->getIsPublished() === false) {
             return;
@@ -32,32 +32,38 @@ class BounceCallbackController extends CommonController
             return;
         }
 
-        //TODO: decide whether to accept json array or single call !!!
-
         $translator = $this->get('translator');
 
-
         $status = $this->request->get('status', 0);
+        $error_message = $this->request->get('error_message', '');
+
+        
 
         if (empty($status)){
             $emailModel = $this->getModel('email');
             $leadModel = $this->getModel('lead');
             $stat  = $emailModel->getEmailStatus($idHash);
 
+            var_dump($stat);
+
             if (!empty($stat)) {
-                $email = $stat->getEmail();
-                $lead = $stat->getLead();
-                $leadModel->setCurrentLead($lead);
+                if (!$stat->isFailed()){
+                    $email = $stat->getEmail();
+                    $lead = $stat->getLead();
+                    $leadModel->setCurrentLead($lead);
 
+                    var_dump($email, $lead);
 
-                /* TODO: 
-                        $emailModel::processMailerCallback 
-                */
+                    /* TODO: 
+                            $emailModel::processMailerCallback 
+                    */
 
-                //TODO if user reached threashold => DoNotContact / Unsubscribe
+                    //TODO if user reached threashold => DoNotContact / Unsubscribe
 
-                //TODO Log bounce event
-                
+                    //TODO Log bounce event
+                }else{
+                    $message = $translator->trans('mautic.plugin.bounce_callback.status.already_reported');    
+                }
             }else{
                 $message = $translator->trans('mautic.email.stat_record.not_found');
             }
@@ -65,130 +71,81 @@ class BounceCallbackController extends CommonController
             $message = $translator->trans('mautic.plugin.bounce_callback.status.notfound');
         }
 
+
 /*
-        if (!empty($stat)) {
-            if ($email = $stat->getEmail()) {
-                $template = $email->getTemplate();
-                if ('mautic_code_mode' === $template) {
-                    // Use system default
-                    $template = null;
-                }
+    public function processMailerCallback(array $response)
+    {
+        if (empty($response)) {
+            return;
+        }
+
+        $statRepo = $this->getStatRepository();
+        $alias    = $statRepo->getTableAlias();
+        if (!empty($alias)) {
+            $alias .= '.';
+        }
+
+        // Keep track to prevent duplicates before flushing
+        $emails = [];
+        $dnc    = [];
+
+        foreach ($response as $type => $entries) {
+            if (!empty($entries['hashIds'])) {
+                $stats = $statRepo->getEntities(
+                    [
+                        'filter' => [
+                            'force' => [
+                                [
+                                    'column' => $alias.'trackingHash',
+                                    'expr'   => 'in',
+                                    'value'  => array_keys($entries['hashIds']),
+                                ],
+                            ],
+                        ],
+                    ]
+                );
 
                 
-                $unsubscribeForm = $email->getUnsubscribeForm();
-                if ($unsubscribeForm != null && $unsubscribeForm->isPublished()) {
-                    $formTemplate = $unsubscribeForm->getTemplate();
-                    $formModel    = $this->getModel('form');
-                    $formContent  = '<div class="mautic-unsubscribeform">'.$formModel->getContent($unsubscribeForm).'</div>';
-                }
-            }
-        }
-
-        if (empty($template) && empty($formTemplate)) {
-            $template = $this->coreParametersHelper->getParameter('theme');
-        } elseif (!empty($formTemplate)) {
-            $template = $formTemplate;
-        }
-
-        $theme = $this->factory->getTheme($template);
-        if ($theme->getTheme() != $template) {
-            $template = $theme->getTheme();
-        }
-        $contentTemplate = $this->factory->getHelper('theme')->checkForTwigTemplate(':'.$template.':message.html.php');
-
-        if (!empty($stat)) {
-            $lead = $stat->getLead();
-            if ($lead) {
-                // Set the lead as current lead
-                $leadModel->setCurrentLead($lead);
-            }
-            // Set lead lang
-            if ($lead->getPreferredLocale()) {
-                $translator->setLocale($lead->getPreferredLocale());
-            }
-
-            if (!$this->get('mautic.helper.core_parameters')->getParameter('show_contact_preferences')) {
-                $message = $this->getUnsubscribeMessage($idHash, $model, $stat, $translator);
-            } elseif ($lead) {
-                $action = $this->generateUrl('mautic_email_unsubscribe', ['idHash' => $idHash]);
-
-                $viewParameters = [
-                    'lead'                         => $lead,
-                    'idHash'                       => $idHash,
-                    'showContactFrequency'         => $this->get('mautic.helper.core_parameters')->getParameter('show_contact_frequency'),
-                    'showContactPauseDates'        => $this->get('mautic.helper.core_parameters')->getParameter('show_contact_pause_dates'),
-                    'showContactPreferredChannels' => $this->get('mautic.helper.core_parameters')->getParameter('show_contact_preferred_channels'),
-                    'showContactCategories'        => $this->get('mautic.helper.core_parameters')->getParameter('show_contact_categories'),
-                    'showContactSegments'          => $this->get('mautic.helper.core_parameters')->getParameter('show_contact_segments'),
-                ];
-
-                $form = $this->getFrequencyRuleForm($lead, $viewParameters, $data, true, $action);
-                if (true === $form) {
-                    return $this->postActionRedirect(
-                        [
-                            'returnUrl'       => $this->generateUrl('mautic_email_unsubscribe', ['idHash' => $idHash]),
-                            'viewParameters'  => $viewParameters,
-                            'contentTemplate' => $contentTemplate,
-                        ]
-                    );
-                }
-
-                $formView = $form->createView();
-               
-                if ($email && ($prefCenter = $email->getPreferenceCenter()) && ($prefCenter->getIsPreferenceCenter())) {
-                    $html = $prefCenter->getCustomHtml();
-                    // check if tokens are present
-                    $savePrefsPresent = false !== strpos($html, 'data-slot="saveprefsbutton"') ||
-                                        false !== strpos($html, BuilderSubscriber::saveprefsRegex);
-                    if ($savePrefsPresent) {
-                        // set custom tag to inject end form
-                        // update show pref center slots by looking for their presence in the html
-                        $params = array_merge(
-                            $viewParameters,
-                            [
-                                'form'                         => $formView,
-                                'custom_tag'                   => '<a name="end-'.$formView->vars['id'].'"></a>',
-                                'showContactFrequency'         => false !== strpos($html, 'data-slot="channelfrequency"') || false !== strpos($html, BuilderSubscriber::channelfrequency),
-                                'showContactSegments'          => false !== strpos($html, 'data-slot="segmentlist"') || false !== strpos($html, BuilderSubscriber::segmentListRegex),
-                                'showContactCategories'        => false !== strpos($html, 'data-slot="categorylist"') || false !== strpos($html, BuilderSubscriber::categoryListRegex),
-                                'showContactPreferredChannels' => false !== strpos($html, 'data-slot="preferredchannel"') || false !== strpos($html, BuilderSubscriber::preferredchannel),
-                            ]
-                        );
-                        // Replace tokens in preference center page
-                        $event = new PageDisplayEvent($html, $prefCenter, $params);
-                        $this->get('event_dispatcher')
-                             ->dispatch(PageEvents::PAGE_ON_DISPLAY, $event);
-                        $html = $event->getContent();
-                        $html = preg_replace('/'.BuilderSubscriber::identifierToken.'/', $lead->getPrimaryIdentifier(), $html);
-                    } else {
-                        unset($html);
+                foreach ($stats as $s) {
+                    $reason = $entries['hashIds'][$s->getTrackingHash()];
+                    if ($this->translator->hasId('mautic.email.bounce.reason.'.$reason)) {
+                        $reason = $this->translator->trans('mautic.email.bounce.reason.'.$reason);
                     }
-                }
 
-                if (empty($html)) {
-                    $html = $this->get('mautic.helper.templating')->getTemplating()->render(
-                        'MauticEmailBundle:Lead:preference_options.html.php',
-                        array_merge(
-                            $viewParameters,
-                            [
-                                'form'         => $formView,
-                                'currentRoute' => $this->generateUrl(
-                                    'mautic_contact_action',
-                                    [
-                                        'objectAction' => 'contactFrequency',
-                                        'objectId'     => $lead->getId(),
-                                    ]
-                                ),
-                            ]
-                        )
-                    );
+                    $dnc[] = $this->setDoNotContact($s, $reason, $type);
+
+                    $s->setIsFailed(true);
+                    $this->em->persist($s);
                 }
-                $message = $html;
             }
-        } else {
-            $message = $translator->trans('mautic.email.stat_record.not_found');
+
+            if (!empty($entries['emails'])) {
+                foreach ($entries['emails'] as $email => $reason) {
+                    if (in_array($email, $emails)) {
+                        continue;
+                    }
+                    $emails[] = $email;
+
+                    $leadId = null;
+                    if (is_array($reason)) {
+                        // Includes a lead ID
+                        $leadId = $reason['leadId'];
+                        $reason = $reason['reason'];
+                    }
+
+                    if ($this->translator->hasId('mautic.email.bounce.reason.'.$reason)) {
+                        $reason = $this->translator->trans('mautic.email.bounce.reason.'.$reason);
+                    }
+
+                    $dnc = array_merge($dnc, $this->setEmailDoNotContact($email, $type, $reason, true, $leadId));
+                }
+            }
         }
+
+        return $dnc;
+    }
  */
+
 
 
 
@@ -237,10 +194,18 @@ class BounceCallbackController extends CommonController
         }
 */
 
-        return new JsonResponse([
-            'status'  => $status,
-            'leadId'  => $leadId,
-            'emailId' => $emailId
-        ]);
+        if (!empty($message)){
+            return new JsonResponse([
+                'success' => true,
+                'message'  => $message,                
+            ]);
+        }else{
+            return new JsonResponse([
+                'success' => true,
+                'status'  => $status,
+                'leadId'  => $leadId,
+                'emailId' => $emailId
+            ]);
+        }
     }
 }
