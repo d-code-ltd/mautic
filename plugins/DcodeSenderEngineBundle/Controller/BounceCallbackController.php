ff9ef52175acb57c4bd9d636098f8bd4b19e0e70
Unknown <peter.osvath@d-code.hu>
2018-11-15 15:11:40 +0100

    bounce callback handling

--- plugins/DcodeSenderEngineBundle/Controller/BounceCallbackController.php
+++ plugins/DcodeSenderEngineBundle/Controller/BounceCallbackController.php
@@ -36,10 +36,8 @@ class BounceCallbackController extends CommonController
 
         $status = $this->request->get('status', 0);
         $error_message = $this->request->get('error_message', '');
-
-        var_dump($status);
-
-        if (empty($status)){
+        
+        if (!empty($status)){
             $emailModel = $this->getModel('email');
             $leadModel = $this->getModel('lead');
             $stat  = $emailModel->getEmailStatus($idHash);
