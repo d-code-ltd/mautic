3504eb22bd97df8a320464e71a772d616b93fc8a
Unknown <peter.osvath@d-code.hu>
2018-12-07 16:43:32 +0100

    senderengine description

--- plugins/DcodeSenderEngineBundle/Integration/SenderEngineIntegration.php
+++ plugins/DcodeSenderEngineBundle/Integration/SenderEngineIntegration.php
@@ -51,6 +51,11 @@ class SenderEngineIntegration extends AbstractIntegration
         return 'SenderEngine';
     }
 
+    public function getDescription()
+    {
+        return 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    }
+
     public function getIcon()
     {
         return 'plugins/DcodeSenderEngineBundle/Assets/img/800px_COLOURBOX11423746.jpg';
