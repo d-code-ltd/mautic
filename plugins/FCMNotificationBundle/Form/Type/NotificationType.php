f98fc6c4db19ef738fee931ac9a2b15618707781
Unknown <peter.osvath@d-code.hu>
2019-07-24 15:26:17 +0200

    fix

--- plugins/FCMNotificationBundle/Form/Type/NotificationType.php
+++ plugins/FCMNotificationBundle/Form/Type/NotificationType.php
@@ -17,6 +17,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
+use Symfony\Component\Validator\Constraints\NotBlank;
+
 /**
  * Class NotificationType.
  */
